74fd7c0b482cc348b60b51770e52b28b335ad40f
Star Poon <ypoon@yahoo-corp.jp>
2022-08-31 12:35:42 +0900

    Update to Hadoop 3.3.5

--- src/main/java/org/apache/hadoop/fs/ForwardingFileSystemCache.java
+++ src/main/java/org/apache/hadoop/fs/ForwardingFileSystemCache.java
@@ -28,6 +28,7 @@ final class ForwardingFileSystemCache
 
     public ForwardingFileSystemCache(FileSystemCache cache)
     {
+        super(new Configuration(false));
         this.cache = requireNonNull(cache, "cache is null");
     }
 
